e447dc77d56d07795fe930b4ef70768686f027d3
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-02-13 13:39:38 +0000

    time & space complexity

--- Brute_Force/linear_search.py
+++ Brute_Force/linear_search.py
@@ -7,8 +7,8 @@ def linear_search(data: list[int], target: int) -> int:
             return indx
     return -1
 
-data: list = [4, 5, 2, 7, 1, 8]
-target: int = 1
+data: list = [-1,0,3,5,9,12] #[4, 5, 2, 7, 1, 8]
+target: int = 9
 
 result: int = linear_search(data, target)
 if result == -1:
---
+++ Time_Space_Complexity/counting_basic_operations.py
@@ -0,0 +1,7 @@
+n = 100  # Assignment statement 1 time 
+count = 0  # Assignment statement 1 time
+while count < n:  # Comparison statement n times 
+    count += 1  # Arithmetic operation (and assignment!) n times + n times
+    print(count)  # Output statement n times
+
+# in total thats 1 + 1 + n + n + n + n = 4n + 2 basic operations
